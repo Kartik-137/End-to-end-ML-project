12f59acb6813f65e94c40b9ab712a3683a3f466c
Kartik Dahake <kartikdahake11@gmail.com>
2025-11-10 00:56:43 +0530

    created logging

---
+++ main.py
@@ -0,0 +1,4 @@
+from red_wine_quality_prediction import logger # Import logger object from the __init_.py file in src/red_wine_quality_prediction/logging 
+
+logger.info("This is the custom log!")
+logger.info("Another log entry!")
---
+++ src/red_wine_quality_prediction/__init__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import logging
+
+# Define logging string format
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs" # Directory to store log files
+
+# Define log file path
+log_filepath = os.path.join(log_dir, "running_log.log")
+os.makedirs(log_dir, exist_ok=True)
+
+# Configure logging
+logging.basicConfig(
+    level = logging.INFO,
+    format = logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath), # Creates log folder on the machine
+        logging.StreamHandler(sys.stdout) # to log on the terminal as well
+    ]
+)
+
+logger = logging.getLogger("red_wine_quality_prediction_logger")
