550240aebce1b2c695d8abb0c132ce138c410d88
Kartik Dahake <kartikdahake11@gmail.com>
2025-11-12 19:02:19 +0530

    new log

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from red_wine_quality_prediction import logger # Import logger object from the __init_.py file in src/red_wine_quality_prediction/logging 
+from red_wine_quality_prediction import logger # Import logger object from the __init_.py file in src/red_wine_quality_prediction
 
 logger.info("This is the custom log!")
-logger.info("Another log entry!")
